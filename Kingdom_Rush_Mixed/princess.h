4187f24dbb9eaa6b15769f2804cba3fdcaf86a99
RUC-Sherlock <ruc.remix@gmail.com>
2018-05-20 23:00:26 +0800

    new version

--- Kingdom_Rush_Mixed/princess.h
+++ Kingdom_Rush_Mixed/princess.h
@@ -7,18 +7,19 @@
 class Princess : public Live_player
 {
 public:
-    enum Command{Lstand,Rstand,Longattack};
-    Princess(int xpos,int ypos);
+    enum Command{Lstand,Rstand,Longattack,Move};
+    Princess(int xpos, int ypos);
     Princess(const QPoint & obj_location);
     void react(Command cmd);
     void draw(QPainter &p);
     bool isLeft(void)const;
-    bool isStanding(void)const {return _condition<2;}
+    bool isStanding(void)const {return _condition<l_longattack1;}
+    bool isMoving(void)const {return _condition==rmove1||_condition==lmove1;}
     bool isRight(void)const {return !isLeft();}
 private:
     enum Condition{lstand1,rstand1,l_longattack1,l_longattack2,
         l_longattack3,l_longattack4,r_longattack1,r_longattack2,
-        r_longattack3,r_longattack4};
+        r_longattack3,r_longattack4,lmove1,rmove1};
     Condition _condition;
 };
 
